be971a3d218e99cbe3cb3244dccb04da126fe842
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-15 18:51:39 -0400

    Adding Colour Display Code Component, styling

--- src/components/ColourDisplay.js
+++ src/components/ColourDisplay.js
@@ -3,21 +3,23 @@ import styled from 'styled-components';
 
 import ColourDisplayItem from './ColourDisplayItem';
 import ColourDisplayList from './ColourDisplayList';
-// import ColourDisplayCode from './ColourDisplayCode'
+import ColourDisplayCode from './ColourDisplayCode';
 
 const Container = styled.div`
     display: grid;
     grid-template-columns: 5rem minmax(2rem, 20rem);
+    .colour-secondary-content {
+        display: grid;
+        align-content: space-between;
+    }
 `;
 
 const ColourDisplay = () => {
     return (
         <Container className="colour-display">
-            <div>
-                <ColourDisplayItem size="4rem" />
-            </div>
-            <div>
-                <div>hsl(120, 100%, 25%)</div>
+            <ColourDisplayItem size="4rem" />
+            <div className="colour-secondary-content">
+                <ColourDisplayCode />
                 <ColourDisplayList />
             </div>
         </Container>
---
+++ src/components/ColourDisplayCode.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const ColourDisplayCode = () => {
+    return <Container>hsl(120, 100%, 25%)</Container>;
+};
+
+export default ColourDisplayCode;

ce021bdaf8710b628eaa3f2b5e8295b3291c7d20
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-16 21:12:26 -0400

    Added global state store through Context API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import ColourCardList from './ColourCardList';
+import StoreContext from './StoreContext';
 
 const App = () => {
+    const [colourCode, setColourCode] = useState({
+        hue: 240,
+        saturation: 50,
+        lightness: 50,
+    });
+    const store = useMemo(
+        () => ({
+            colourCode,
+            setColourCode,
+        }),
+        [colourCode, setColourCode]
+    );
+
     return (
-        <div className="container">
-            <ColourCardList />
-        </div>
+        <StoreContext.Provider value={store}>
+            <div className="container">
+                <ColourCardList />
+            </div>
+        </StoreContext.Provider>
     );
 };
 

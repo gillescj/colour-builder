7fb2e4c8f530dbc64052f2f894331e0d4ac692bc
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-20 22:47:16 -0400

    Added copy to clipboard button.

--- src/components/ColourDisplayCode.js
+++ src/components/ColourDisplayCode.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 
 import colourCodeObjectToCSS from '../utils/colourCodeObjectToCSS';
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 0.5rem;
+    input {
+        background: none;
+        border: none;
+        /* width: auto; */
+    }
+`;
 
 const ColourDisplayCode = ({ colourCode }) => {
     const cssColourCode = colourCodeObjectToCSS(colourCode);
-    return <Container>{cssColourCode}</Container>;
+    const colourCodeInputRef = useRef();
+
+    const handleButtonCopy = (event) => {
+        event.preventDefault();
+        event.clipboardData.setData('text/plain', 'test');
+    };
+
+    const handleButtonClick = () => {
+        colourCodeInputRef.current.select();
+        document.execCommand('copy');
+    };
+
+    return (
+        <Container>
+            <button
+                onCopy={(event) => handleButtonCopy(event)}
+                onClick={() => handleButtonClick()}
+            >
+                Copy
+            </button>
+            <input ref={colourCodeInputRef} value={cssColourCode} readonly />
+        </Container>
+    );
 };
 
 export default ColourDisplayCode;

8b7cc02995c33aac2b6185ad3fffe0c6f984b993
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-22 18:52:13 -0400

    styledComponents to hold global component styles

---
+++ src/styles/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    padding: 0.5rem 0rem;
+    border: none;
+    background: hsl(228, 100%, 65%);
+    font-size: 1rem;
+    border-radius: 0.5rem;
+    color: hsl(199, 85%, 95%);
+    cursor: pointer;
+`;

7fa49fa1110af38e895dac690d67cb060f421840
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-22 16:51:15 -0400

    Changed styles box-shadow, grid-gap padding, border-radius

--- src/components/ColourCard.js
+++ src/components/ColourCard.js
@@ -6,10 +6,13 @@ import ColourInput from './ColourInput';
 import ColourNameHeader from './ColourNameHeader';
 
 const Container = styled.div`
+    display: grid;
+    grid-gap: 1rem;
     padding: 1rem;
     border: none;
-    border-radius: 0.5rem;
+    border-radius: 1.5rem;
     background: hsl(0, 0%, 100%);
+    box-shadow: 0 5rem 3rem -5rem rgba(0, 0, 0, 0.35);
 `;
 
 const ColourCard = () => {

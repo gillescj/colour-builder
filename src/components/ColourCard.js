b49c5772f273cc561adde893f2ad6ea0fcc0e5da
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-25 18:34:45 -0400

    Dynamic box shadow and border colour change

--- src/components/ColourCard.js
+++ src/components/ColourCard.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 
 import ColourDisplay from './ColourDisplay';
 import ColourInput from './ColourInput';
 import ColourNameHeader from './ColourNameHeader';
+import StoreContext from './StoreContext';
+import colourCodeObjectToCSS from '../utils/colourCodeObjectToCSS';
 
-const Container = styled.div`
+const Container = styled.div.attrs((props) => ({
+    style: {
+        boxShadow: `-10px 10px 0 ${props.cssColourCode}`,
+        borderBottom: `1px solid ${props.cssColourCode}`,
+        borderLeft: `1px solid ${props.cssColourCode}`,
+    },
+}))`
     display: grid;
     grid-gap: 1rem;
     padding: 1rem;
     background: hsl(0, 0%, 100%);
-    box-shadow: -10px 10px 0 black;
-    border: 1px solid black;
+    border-top: 1px solid black;
+    border-right: 1px solid black;
 `;
 
 const MainContent = styled.div`
@@ -25,8 +33,10 @@ const MainContent = styled.div`
 `;
 
 const ColourCard = () => {
+    const { colourCode } = useContext(StoreContext);
+
     return (
-        <Container className="colour-card">
+        <Container cssColourCode={colourCodeObjectToCSS(colourCode)}>
             <ColourNameHeader />
             <MainContent>
                 <ColourDisplay />

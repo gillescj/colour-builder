677829379a28113988e9defc95f07326472ee426
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-25 17:20:14 -0400

    Added Save Button with no functionality

--- src/components/ColourNameHeader.js
+++ src/components/ColourNameHeader.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { ReactComponent as SaveLogo } from '../assets/svgs/save.svg';
+import { Button } from '../styles/styledComponents';
 
 const Container = styled.header`
     display: grid;
-    grid-template-columns: minmax(1rem, 15rem);
+    grid-template-columns: minmax(1rem, 15rem) auto;
     input {
+        justify-self: start;
         background: none;
         border: none;
         font-size: 1.5rem;
+    }
+`;
+
+const SaveButton = styled(Button)`
+    justify-self: end;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem;
+    background: hsl(340, 82%, 52%);
+    svg {
+        fill: white;
+        width: 2rem;
     }
 `;
 
@@ -26,6 +42,9 @@ const ColourNameHeader = () => {
                 placeholder="Colour Name"
                 onChange={(event) => handleColourNameInputChange(event)}
             />
+            <SaveButton>
+                <SaveLogo />
+            </SaveButton>
         </Container>
     );
 };

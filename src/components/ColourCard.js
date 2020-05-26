500ce0ec6d973d55b5a4d4f26e4e92bcb08c1fe0
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-26 01:45:44 -0400

    Border style change Thicker and not fully black

--- src/components/ColourCard.js
+++ src/components/ColourCard.js
@@ -10,16 +10,13 @@ import colourCodeObjectToCSS from '../utils/colourCodeObjectToCSS';
 const Container = styled.div.attrs((props) => ({
     style: {
         boxShadow: `-10px 10px 0 ${props.cssColourCode}`,
-        borderBottom: `1px solid ${props.cssColourCode}`,
-        borderLeft: `1px solid ${props.cssColourCode}`,
     },
 }))`
     display: grid;
     grid-gap: 1rem;
     padding: 1rem;
     background: hsl(0, 0%, 100%);
-    border-top: 1px solid black;
-    border-right: 1px solid black;
+    border: 3px solid hsl(60, 6%, 19%);
 `;
 
 const MainContent = styled.div`

c963b6a170075e89cc44c47c03dc1ac036b8ae8c
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-31 21:54:09 -0400

    Added colourList array to global state Using dumby values

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import MainContainer from './MainContainer';
 import StoreContext from './StoreContext';
+import _ from 'lodash';
 
 const App = () => {
     const [colourCode, setColourCode] = useState({
@@ -8,12 +9,67 @@ const App = () => {
         saturation: 50,
         lightness: 50,
     });
+
+    const [colourList, setColourList] = useState([
+        {
+            id: _.uniqueId(),
+            name: 'Cool Orange',
+            hue: 25,
+            saturation: 65,
+            lightness: 50,
+        },
+        {
+            id: _.uniqueId(),
+            name: 'Stylish Sugar Red',
+            hue: 0,
+            saturation: 70,
+            lightness: 50,
+        },
+        {
+            id: _.uniqueId(),
+            name: 'Just Blue',
+            hue: 220,
+            saturation: 70,
+            lightness: 50,
+        },
+        {
+            id: _.uniqueId(),
+            name: 'Country Wide Lavender',
+            hue: 280,
+            saturation: 70,
+            lightness: 60,
+        },
+        {
+            id: _.uniqueId(),
+            name: 'Tan Burnt Brown',
+            hue: 40,
+            saturation: 55,
+            lightness: 35,
+        },
+        {
+            id: _.uniqueId(),
+            name: 'Simple Cyan',
+            hue: 180,
+            saturation: 100,
+            lightness: 50,
+        },
+        {
+            id: _.uniqueId(),
+            name: 'Fromage Yellow',
+            hue: 60,
+            saturation: 80,
+            lightness: 55,
+        },
+    ]);
+
     const store = useMemo(
         () => ({
             colourCode,
             setColourCode,
+            colourList,
+            setColourList,
         }),
-        [colourCode, setColourCode]
+        [colourCode, setColourCode, colourList, setColourList]
     );
 
     return (

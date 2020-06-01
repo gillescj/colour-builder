5a9872be32df765a54de6e5227e2882c0cd6b228
Colin Gillespie <ratchetandcolin@gmail.com>
2020-05-31 22:46:13 -0400

    Style Change - on Hover, shadow coloured

--- src/components/savedColours/SavedColourItem.js
+++ src/components/savedColours/SavedColourItem.js
@@ -3,19 +3,27 @@ import styled from 'styled-components';
 
 const Container = styled.button.attrs((props) => ({
     style: {
-        background: props.selected ? 'hsl(60, 6%, 19%)' : 'white',
-        color: props.selected ? 'white' : 'hsl(60, 6%, 19%)',
+        background: props.selected ? 'hsl(0, 0%, 13%)' : '',
+        color: props.selected ? 'white' : '',
+        boxShadow: `-3px 3px 0 ${props.colourCodeCSS}`,
     },
 }))`
-    border: 3px solid hsl(60, 6%, 19%);
+    border: 3px solid hsl(0, 0%, 13%);
     padding: 0.25rem;
     cursor: pointer;
-    box-shadow: -3px 3px 0 hsl(60, 6%, 19%);
     margin: 0 10px 10px 0;
+    &:hover {
+        background: hsl(60, 6%, 19%);
+        color: white;
+    }
 `;
 
-const SavedColourItem = ({ name, selected }) => {
-    return <Container selected={selected}>{name}</Container>;
+const SavedColourItem = ({ name, selected, colourCodeCSS }) => {
+    return (
+        <Container selected={selected} colourCodeCSS={colourCodeCSS}>
+            {name}
+        </Container>
+    );
 };
 
 export default SavedColourItem;

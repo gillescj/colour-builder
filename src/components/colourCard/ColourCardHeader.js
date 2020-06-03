963689f838db8d815165319a55bc5c5b817381e1
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-02 21:24:33 -0400

    Moved SaveButton into seperate component

--- src/components/colourCard/ColourCardHeader.js
+++ src/components/colourCard/ColourCardHeader.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as HeartSVG } from '../../assets/svgs/8-bit-heart.svg';
-import { ReactComponent as BrokenHeartSVG } from '../../assets/svgs/8-bit-heart-broken.svg';
 import ColourNameInput from './ColourNameInput';
+import SaveButton from './SaveButton';
 
 const Container = styled.header`
     display: grid;
@@ -10,46 +9,11 @@ const Container = styled.header`
     grid-gap: 1rem;
 `;
 
-const SaveButton = styled.div`
-    justify-self: end;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const SaveLogo = styled(HeartSVG)`
-    width: 3rem;
-    height: 3rem;
-    fill: hsl(0, 0%, 13%);
-    cursor: pointer;
-    filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
-
-    &:hover {
-        fill: hsl(340, 80%, 45%);
-        filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
-    }
-`;
-
-const UnsaveLogo = styled(BrokenHeartSVG)`
-    width: 3rem;
-    height: 3rem;
-    fill: hsl(0, 0%, 13%);
-    cursor: pointer;
-    filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
-
-    &:hover {
-        fill: hsl(340, 80%, 45%);
-        filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
-    }
-`;
-
 const ColourCardHeader = ({ saved }) => {
-    const renderSaveLogo = saved ? <UnsaveLogo /> : <SaveLogo />;
-
     return (
         <Container>
             <ColourNameInput />
-            <SaveButton>{renderSaveLogo}</SaveButton>
+            <SaveButton saved={saved} />
         </Container>
     );
 };
---
+++ src/components/colourCard/SaveButton.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as HeartSVG } from '../../assets/svgs/8-bit-heart.svg';
+import { ReactComponent as BrokenHeartSVG } from '../../assets/svgs/8-bit-heart-broken.svg';
+
+const Container = styled.div`
+    justify-self: end;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const SaveLogo = styled(HeartSVG)`
+    width: 3rem;
+    height: 3rem;
+    fill: hsl(0, 0%, 13%);
+    cursor: pointer;
+    filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
+
+    &:hover {
+        fill: hsl(340, 80%, 45%);
+        filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
+    }
+`;
+
+const UnsaveLogo = styled(BrokenHeartSVG)`
+    width: 3rem;
+    height: 3rem;
+    fill: hsl(0, 0%, 13%);
+    cursor: pointer;
+    filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
+
+    &:hover {
+        fill: hsl(340, 80%, 45%);
+        filter: drop-shadow(-3px 3px 0 hsl(340, 60%, 59%));
+    }
+`;
+
+const SaveButton = ({ saved }) => {
+    const renderSaveLogo = saved ? <UnsaveLogo /> : <SaveLogo />;
+
+    return <Container>{renderSaveLogo}</Container>;
+};
+
+export default SaveButton;

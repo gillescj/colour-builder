25a8bdfbafebb030098ff26bfbaaa33117198541
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-06 21:55:25 -0400

    Remove dummy variables

--- src/components/App.js
+++ src/components/App.js
@@ -14,57 +14,10 @@ const App = () => {
 
     const [selectedColourSaved, setSelectedColourSaved] = useState(false);
 
-    const [savedColoursList, setSavedColoursList] = useLocalStorage('savedColoursList', [
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Cool Orange',
-        //     hue: 25,
-        //     saturation: 65,
-        //     lightness: 50,
-        // },
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Stylish Sugar Red',
-        //     hue: 0,
-        //     saturation: 70,
-        //     lightness: 50,
-        // },
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Just Blue',
-        //     hue: 220,
-        //     saturation: 70,
-        //     lightness: 50,
-        // },
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Country Wide Lavender',
-        //     hue: 280,
-        //     saturation: 70,
-        //     lightness: 60,
-        // },
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Tan Burnt Brown',
-        //     hue: 40,
-        //     saturation: 55,
-        //     lightness: 35,
-        // },
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Simple Cyan',
-        //     hue: 180,
-        //     saturation: 100,
-        //     lightness: 50,
-        // },
-        // {
-        //     id: _.uniqueId(),
-        //     name: 'Fromage Yellow',
-        //     hue: 60,
-        //     saturation: 80,
-        //     lightness: 55,
-        // },
-    ]);
+    const [savedColoursList, setSavedColoursList] = useLocalStorage(
+        'savedColoursList',
+        []
+    );
 
     const [focusColourNameInput, setFocusColourNameInput] = useState(false);
 
